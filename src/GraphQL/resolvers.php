ce40ebbb75c3ecad3918d6308667d84e5b7f1cad
Mostafa <mostafatarek7000@gmail.com>
2023-08-03 01:59:28 +0300

    add mutation to shcema

--- src/GraphQL/resolvers.php
+++ src/GraphQL/resolvers.php
@@ -20,5 +20,23 @@ return [
                 ]
             ];
         },
-    ]
+    ],
+    'Mutation' => [
+        'createBook' => function ($root, $args, $context) {
+            // Extract the input data from the arguments
+            $bookData = $args['input'];
+
+            // Create the new Book object with the provided data
+            $newBook = [
+                'id' => uniqid(), // You can generate a unique ID using your preferred method
+                'title' => $bookData['title'],
+                'authorId' => $bookData['authorId'],
+            ];
+
+            // In a real application, I would save the new book to database or storage
+
+            // Return the newly created Book
+            return $newBook;
+        },
+    ],
 ];
